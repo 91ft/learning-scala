54e1ebdf5028066f7a1522757d3103dccf5c4068
vies <karellen.kim@daumkakao.com>
2016-03-09 16:09:22 +0900

    chapter 6

--- src/main/scala/ScalafortheImpatient/ch06/_8_Main3.scala
+++ src/main/scala/ScalafortheImpatient/ch06/_8_Main3.scala
@@ -4,13 +4,16 @@ package ScalafortheImpatient.ch06
  * Created by vies00 on 16. 3. 9..
  */
 object _8_Main3 extends MyApp {
-  println("Hello MyApp!")
+  println("Hello MyApp!!")
 }
 
+
 trait MyApp extends DelayedInit {
-  var x: Function0[Unit] = null
+  var x : Any = null
   override def delayedInit(cons: => Unit) {
-    x = () => cons
+    x = cons
   }
-  def main(args: Array[String]): Unit = x()
+  def main(args: Array[String]): Unit = {
+    x
+  }
 }
